4a9ccaa0bbf5354ee2601ccb894d0ae2be507537
mdtomasuddin <mdtomasuddin1@gmail.com>
2026-01-22 20:15:41 +0600

    Blog Category API

--- routes/Api/api.php
+++ routes/Api/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\V1\CategoryFeature\AmenitiesController;
 use App\Http\Controllers\Api\V1\CategoryFeature\BillIncludedsController;
+use App\Http\Controllers\Api\V1\CategoryFeature\BlogCategoryController;
 use App\Http\Controllers\Api\V1\CityFeature\CityController;
 use App\Http\Controllers\Api\V1\FAQ\FAQController;
 use App\Http\Controllers\Api\V1\Property\PropertyController;
@@ -18,3 +19,4 @@ Route::apiResource('city', CityController::class);
 Route::apiResource('amenitie', AmenitiesController::class);
 Route::apiResource('bill-included', BillIncludedsController::class);
 Route::apiResource('property-type', PropertyTypesController::class);
+Route::apiResource('blog-category', BlogCategoryController::class);
