dc9c2a30959209881121aea45ae64db182d3f409
mdtomasuddin <mdtomasuddin1@gmail.com>
2026-01-22 19:19:58 +0600

    bill Included API

--- routes/Api/api.php
+++ routes/Api/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\CategoryFeature\AmenitiesController;
+use App\Http\Controllers\Api\V1\CategoryFeature\BillIncludedsController;
 use App\Http\Controllers\Api\V1\CityFeature\CityController;
 use App\Http\Controllers\Api\V1\FAQ\FAQController;
 use App\Http\Controllers\Api\V1\Property\PropertyController;
@@ -10,6 +11,8 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('faq', FAQController::class);
 
 // property
-Route::apiResource('property', PropertyController::class);
+// Route::apiResource('property', PropertyController::class);
 Route::apiResource('city', CityController::class);
+//Category Feature 
 Route::apiResource('amenitie', AmenitiesController::class);
+Route::apiResource('bill-included', BillIncludedsController::class);
