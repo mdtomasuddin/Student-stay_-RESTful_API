31eb82f305d714968ef60bd73bd9ac486a5699e0
mdtomasuddin <mdtomasuddin1@gmail.com>
2026-01-28 12:01:20 +0600

    partnerPage hero featues

--- resources/views/backend/partials/sidebar.blade.php
+++ resources/views/backend/partials/sidebar.blade.php
@@ -73,6 +73,14 @@
                         <span data-key="t-student-blog-hero">Student Blog Hero Banner</span>
                     </a>
                 </li>
+                {{-- partner-page-hero --}}
+                <li class="nav-item">
+                    <a href="{{ route('partner-page-hero.index') }}"
+                        class="nav-link menu-link {{ request()->routeIs('partner-page-hero.*') ? 'active' : '' }}">
+                        <i class="ri-image-line"></i>
+                        <span data-key="t-partner-page-hero">Partner Page Hero Banner</span>
+                    </a>
+                </li>
                 <hr style="border: none; height: 3px; background-color: hsl(0, 8%, 64%); margin-bottom: 5px;">
                 {{-- manage-properties --}}
                 <li class="nav-item">
--- routes/Web/web.php
+++ routes/Web/web.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\web\Backend\V1\CategoryFeature\ReferralSourceCategoryCo
 use App\Http\Controllers\web\Backend\V1\CategoryFeature\RoomTypeCategoryController;
 use App\Http\Controllers\Web\Backend\V1\CityFeatures\CityController;
 use App\Http\Controllers\Web\Backend\V1\CMS\HomePage\HeroBannerController;
+use App\Http\Controllers\Web\Backend\V1\CMS\PartnerPage\PartnerPageHeroBannerController;
 use App\Http\Controllers\Web\Backend\V1\CMS\StudentBlog\StudentBlogHeroBannerController;
 use App\Http\Controllers\Web\Backend\V1\DigitalResourceFeatures\DigitalResourceAccessController;
 use App\Http\Controllers\Web\Backend\V1\DigitalResourceFeatures\DigitalResourceController;
@@ -94,3 +95,4 @@ Route::resource('testimonials', TestimonialController::class);
 // Hero Banner Routes
 Route::resource('homepage-hero', HeroBannerController::class); //Home Page Hero section
 Route::resource('student-blog-hero', StudentBlogHeroBannerController::class); //Student Blog Hero section
+Route::resource('partner-page-hero', PartnerPageHeroBannerController::class); //Partner Page Hero section
