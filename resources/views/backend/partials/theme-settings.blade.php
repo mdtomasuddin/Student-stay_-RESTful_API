45e6672d7a5ec0b4be68f58759625b0705eeecfb
@tomas15691 <tomas@softvence.com>
2026-04-20 11:56:08 +0600

    Dashboard Customization

--- resources/views/backend/partials/theme-settings.blade.php
+++ resources/views/backend/partials/theme-settings.blade.php
@@ -1,11 +1,4 @@
-{{-- Customizer Button --}}
-<div class="customizer-setting d-none d-md-block">
-    <div class="btn btn-info p-2 text-uppercase rounded-end-0 shadow-lg" data-bs-toggle="offcanvas"
-        data-bs-target="#theme-settings-offcanvas" aria-controls="theme-settings-offcanvas">
-        <i class="bi bi-gear mb-1"></i> Customizer
-    </div>
-</div>
-{{-- Customizer Button --}}
+
 
 
 <div class="offcanvas offcanvas-end border-0" tabindex="-1" id="theme-settings-offcanvas">
