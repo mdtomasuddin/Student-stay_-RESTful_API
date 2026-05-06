a136ab3e5326a82e3983989efca26e4f1f939943
mdtomasuddin <mdtomsuddin1@gmail.com>
2026-05-06 09:36:33 +0600

    SeoMeta ms

---
+++ app/Models/SeoMeta.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SeoMeta extends Model
+{
+    //table
+    protected $table = 'seo_metas';
+
+    //allow mass assignment
+    protected $guarded = [];
+
+    //hidden fields
+    protected $hidden = ['updated_at'];
+
+    //casts
+    protected $casts = [
+        'id'          => 'integer',
+        'page'        => 'string',
+        'title'       => 'string',
+        'description' => 'string',
+        'keywords'    => 'array',
+        'created_at'  => 'datetime',
+        'updated_at'  => 'datetime',
+    ];
+}
